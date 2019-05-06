5b2afdf655296af7a4541352e1474bbc930f49bc
Declan Galleher <declangalleher@ip-124-211.wireless.oberlin.edu>
2019-05-05 20:20:19 -0400

    video almost working

--- load_image.py
+++ load_image.py
@@ -21,6 +21,36 @@ def roi(img):
     maskedImg = cv2.bitwise_and(img, mask)
     return maskedImg
 
+def mkCoords(img, line):
+    #print(line)
+    slope,intercept = line
+    y1 = img.shape[0]
+    y2 = int(y1*(3/5))
+    x1 = int((y1-intercept)/slope)
+    x2 = int((y2-intercept)/slope)
+    return np.array([x1, y1, x2, y2])
+
+def avgSlopeInter(img,lines):
+    left = []
+    right = []
+    for line in lines:
+        #print(line)
+        x1, y1, x2, y2 = line.reshape(4)
+        params = np.polyfit((x1,x2), (y1,y2), 1)
+        slope = params[0]
+        intercept = params[1]
+        if slope < 0:
+            left.append((slope, intercept))
+        else:
+            right.append((slope, intercept))
+    leftAvg = np.average(left, axis=0)
+    rightAvg = np.average(right, axis=0)
+    #print(leftAvg)
+    #print(rightAvg)
+    leftLine = mkCoords(img, leftAvg)
+    rightLine = mkCoords(img, rightAvg)
+    return np.array([leftLine, rightLine])
+
 def displayLines(img, lines):
     lineImg = np.zeros_like(img)
     if lines is not None:
@@ -32,7 +62,7 @@ def displayLines(img, lines):
 # display image
 def display(img):
     plt.imshow(img)
-    plt.show()
+    #plt.show()
 
 def display2(img1, img2):
     plt.subplot(121)
@@ -41,13 +71,23 @@ def display2(img1, img2):
     plt.imshow(img2)
     plt.show()
 
-def main():
-    img = readImg()
+def analyzeFrame(frame):
+    img = frame
     cannyImg = canny(img)
     maskedImg = roi(cannyImg)
     lines = cv2.HoughLinesP(maskedImg,2,np.pi/180,100,np.array([]),minLineLength=40,maxLineGap=5)
-    lineImg = displayLines(img, lines)
+    avgLines = avgSlopeInter(img, lines)
+    lineImg = displayLines(img, avgLines)
     addImg = cv2.addWeighted(img, 0.8,lineImg,1,1)
-    display2(img, addImg)
+    #display2(img, addImg)
+    #display(addImg)
+    cv2.imshow("result", addImg)
+    cv2.waitKey(1)
+
+def main():
+    cap = cv2.VideoCapture("test2.mp4")
+    while cap.isOpened():
+        frame = cap.read()[1]
+        analyzeFrame(frame)
 
 main()
